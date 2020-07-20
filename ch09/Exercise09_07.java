8c6e9167e4544db064c7a6045789ec0b5d8886e6
whyhardpickname <13zxchen3@gmail.com>
2020-07-20 16:38:51 +0800

    Exercise09_07

---
+++ ch09/Exercise09_07.java
@@ -0,0 +1,87 @@
+import java.util.Date;
+public class Exercise09_07
+{
+	public static void main(String[] args)
+	{
+		Account account = new Account(1122, 20000.0);
+		
+		account.setAnnualInterestRate(4.5);
+		account.withDraw(2500);
+		account.deposit(3000);
+		
+		double monthlyInterest = account.getMonthlyInterestRate() / 100
+			* account.getBalance();
+		System.out.println("Account[balance=" + account.getBalance()
+			+ ", monthlyInterestRate=" + monthlyInterest 
+			+ ", dateCreated=" + account.getDateCreated());
+	}
+}
+
+class Account
+{
+	private int id = 0;
+	private double balance = 0.0;
+	private double annualInterestRate = 0;
+	private Date dateCreated = new Date();
+	
+	public Account()
+	{
+		;
+	}
+	
+	public Account(int id, double balance)
+	{
+		this.id = id;
+		this.balance = balance;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	public double getAnnualInterestRate()
+	{
+		return annualInterestRate;
+	}
+	
+	public double getMonthlyInterestRate()
+	{
+		return annualInterestRate / 12;
+	}
+	
+	public String getDateCreated()
+	{
+		return dateCreated.toString();
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public void setBalance(double balance)
+	{
+		this.balance = balance;
+	}
+	
+	public void setAnnualInterestRate(double annualInterestRate)
+	{
+		this.annualInterestRate = annualInterestRate;
+	}
+	
+	public void withDraw(double amount)
+	{
+		this.balance -= amount;
+	}
+	
+	public void deposit(double amount)
+	{
+		this.balance += amount;
+	}	
+}
