9dc69a1b25d9e4d24b52d47908ea249cb999dc99
whyhardpickname <13zxchen3@gmail.com>
2020-07-23 17:32:13 +0800

    RegularPolygon.drawio and Exercise09_09

---
+++ ch09/Exercise09_09.java
@@ -0,0 +1,74 @@
+class RegularPolygon
+{
+	private int n = 3;
+	private double side = 1.0;
+	private double x = 0.0;
+	private double y = 0.0;
+	
+	public RegularPolygon()
+	{
+	}
+	
+	public RegularPolygon(int n, double side, double x, double y)
+	{
+		this.n = n;
+		this.side = side;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public RegularPolygon(int n, double side)
+	{
+		this(n, side, 0.0, 0.0);
+	}
+	
+	public int getN()
+	{
+		return n;
+	}
+	
+	public double getSide()
+	{
+		return side;
+	}
+	
+	public double getX()
+	{
+		return x;
+	}
+	
+	public double getY()
+	{
+		return y;
+	}
+	
+	public void	setN(int n)
+	{
+		this.n = n;
+	}
+	
+	public void setSide(double side)
+	{
+		this.side = side;
+	}
+	
+	public void setX(double x)
+	{
+		this.x = x;
+	}
+	
+	public void setY(double y)
+	{
+		this.y = y;
+	}
+	
+	public double getPerimeter()
+	{
+		return n * side;
+	}
+	
+	public double getArea()
+	{
+		return	n * side * side / 4 / Math.tan(Math.PI / n);
+	}
+}
