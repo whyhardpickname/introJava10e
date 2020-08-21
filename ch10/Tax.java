08ea3b7d51ef869e3a63d9e3993e94a76007e2d1
whyhardpickname <13zxchen3@gmail.com>
2020-08-21 11:07:15 +0800

    tax

--- ch10/Tax.java
+++ ch10/Tax.java
@@ -1,4 +1,54 @@
 public class Tax
 {
-	//todo
+	public static final int SINGLE_FILER = 0;
+	public static final int MARRIED_JOINTLY_OR_QUALIFYING_WIDOWER = 1;
+	public static final int MARRIED_SEPARATELY = 2;
+	public static final int HEAD_OF_HOUSEHOLD = 3;
+	private int filingStatus;
+	private int[][] brackets = new in[4][5];
+	private double[] rates = new double[5];
+	private double taxableIncome;
+	
+	public int[][] getBrackets()
+	{
+		return brackets;
+	}
+	
+	public double[] getRates()
+	{
+		return rates;
+	}
+	
+	public double getTaxableIncome()
+	{
+		return taxableIncome;
+	}
+	
+	public double getTax()
+	{
+		double tax = 0;
+		for (int i = 0; i < rates.length; i++)
+		{
+			if (taxableIncome > brackets[filingStatus][i])
+				
+		}
+		
+		
+		return tax;
+	}
+	
+	public void setBrackets(int[][] brackets)
+	{
+		this.brackets = brackets;
+	}
+	
+	public void setRates(double[] rates)
+	{
+		this.rates = rates;
+	}
+	
+	public void setTaxableIncome()
+	{
+		this.taxableIncome = taxableIncome;
+	}
 }
