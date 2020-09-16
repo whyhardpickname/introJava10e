13bbf61d0b60b14883cb9a1957b1390eb4c9d98b
whyhardpickname <13zxchen3@gmail.com>
2020-09-16 14:27:48 +0800

    split realization

--- ch10/Exercise10_25.java
+++ ch10/Exercise10_25.java
@@ -1,50 +1,21 @@
 import java.util.*;
-//to do
 public class Exercise10_25
 {
 	public static void main(String[] args)
 	{
-		String s = "ababab";
+		String s = "abbabbabba";
 		String[] a = split(s, "b");
-		System.out.println(contains(s, "b", 4));
 		for (int i = 0; i < a.length; i++)
 		{
 			System.out.println(a[i]);
 		}
 	}
-	public static String[] split(String s, String regex)
-	{
-		ArrayList<String> result = new ArrayList<String>();
-		
-		int i = 0;
-		while (i < s.length())
-		{
-			int j = contains(s, regex, i);
-			System.out.printf("%d %d\n", i, j);
-			if (j >= 0)
-			{
-				result.add(s.substring(i, j));
-				result.add(regex);
-			}
-			else
-			{
-				result.add(s.substring(i, s.length()));
-				return result.toArray(new String[result.size()]);
-			}
-			i = j + regex.length();
-		}
-		return result.toArray(new String[result.size()]);
-	}		
 	
-	public static int contains(String s, String regex, int from)
+	public static int[] contains(String s, String regex, int from)
 	{
-		if (regex.length() == 0)
-		{
-			return from + 1;
-		}
-		
+		ArrayList<Integer> indexes = new ArrayList<Integer>();
 		boolean match = false;
-		for (int i = from; i + regex.length() < s.length(); i++)
+		for (int i = from; i + regex.length() - 1 < s.length(); i++)
 		{
 			match = true;
 			for (int j = 0; j < regex.length(); j++)
@@ -56,9 +27,31 @@ public class Exercise10_25
 			}
 			if (match)
 			{
-				return i;
+				indexes.add(i);
 			}
 		}
-		return -1;
+		int[] arr = indexes.stream().mapToInt(k->k).toArray();
+		return arr;
 	}
+	
+	public static String[] split(String s, String regex)
+	{
+		int[] indexes = contains(s, regex, 0);
+		if (indexes.length == 0)
+		{
+			return new String[] {s};
+		}
+		ArrayList<String> tokens = new ArrayList<String>();
+		tokens.add(s.substring(0, indexes[0]));
+		tokens.add(regex);
+		for (int i = 1; i < indexes.length; i++)
+		{
+			tokens.add(s.substring(indexes[i - 1] + regex.length(), indexes[i]));
+			tokens.add(regex);
+		}
+		tokens.add(s.substring(indexes[indexes.length - 1] + regex.length(), s.length()));
+		return tokens.toArray(new String[tokens.size()]);
+	}
+	
+	
 }
