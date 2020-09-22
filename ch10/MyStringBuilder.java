37da521b531722c45fd941a1d6f837864305f966
whyhardpickname <13zxchen3@gmail.com>
2020-09-22 11:09:57 +0800

    MyStringBuilder.java

--- ch10/MyStringBuilder.java
+++ ch10/MyStringBuilder.java
@@ -6,7 +6,7 @@ public class MyStringBuilder
 
 	public MyStringBuilder(String s)
 	{
-
+        this(s.toCharArray());
 	}
 	public MyStringBuilder()
 	{
@@ -14,54 +14,118 @@ public class MyStringBuilder
 		capacity = 16;
 		chars = new char[capacity];
 	}
-	public MyStringBuilder(char[] chars)
-	{
-
-	}
+	public MyStringBuilder(char[] chars) {
+        this.capacity = 16 + chars.length;
+        this.size = chars.length;
+        this.chars = new char[this.capacity];
+        for (int i = 0; i < size; i++) {
+            this.chars[i] = chars[i];
+        }
+    }
 	public MyStringBuilder append(MyStringBuilder s)
-	{
-		return null;
+    {
+        return this.append(s.toString());
 	}
+	public MyStringBuilder append(String s)
+    {
+        if (capacity < size + s.length())
+        {
+            while (capacity < size + s.length()) {
+                capacity *= 2;
+            }
+            System.arraycopy(chars, 0, chars, 0, capacity);
+        }
+        for (int i = 0; i < s.length(); i++)
+        {
+            chars[i + size] = s.charAt(i);
+        }
+        size += s.length();
+        return this;
+    }
 	public MyStringBuilder append(int i)
-	{
-		return null;
+    {
+        String str = String.valueOf(i);
+		return this.append(new MyStringBuilder(str));
 	}
 	public MyStringBuilder insert(int offset, MyStringBuilder s)
 	{
-		return null;
+	    this.insert(offset, s.toString());
+		return this;
 	}
+    public MyStringBuilder insert(int offset, String  s)
+    {
+
+        if (capacity < size + s.length())
+        {
+            while (capacity < size + s.length()) {
+                capacity *= 2;
+            }
+            System.arraycopy(chars, 0, chars, 0, capacity);
+        }
+        for (int i = offset; i < size; i++)
+        {
+            chars[i + s.length()] = chars[i];
+        }
+        for (int i = 0; i < s.length(); i++)
+        {
+            chars[i + offset] = s.charAt(i);
+        }
+        size += s.length();
+        return this;
+    }
 	public MyStringBuilder reverse()
 	{
-		return null;
+	    char temp;
+	    for (int i = 0; i < size / 2; i++)
+        {
+            temp = chars[i];
+            chars[i] = chars[size - 1 - i];
+            chars[size - 1 - i] = chars[i];
+        }
+		return this;
 	}
 	public int length()
 	{
 		return size;
 	}
 	public char charAt(int index)
-	{
-		return ' ';
+    {
+		return chars[index];
 	}
 	public MyStringBuilder toLowerCase()
 	{
-		return null;
+	    for (int i = 0; i < this.size; i++)
+        {
+            chars[i] = Character.toLowerCase(chars[i]);
+        }
+		return this;
 	}
 	public MyStringBuilder toUpperCase()
 	{
-		return null;
+        for (int i = 0; i < this.size; i++)
+        {
+            chars[i] = Character.toUpperCase(chars[i]);
+        }
+        return this;
 	}
 	public MyStringBuilder substring(int begin, int end)
 	{
-		return null;
+	    char[] subChars = new char[end - begin];
+	    for (int i = 0; i < subChars.length; i++)
+        {
+            subChars[i] = chars[i + begin];
+        }
+		return new MyStringBuilder(subChars);
 	}
 	public String toString()
 	{
-		return null;
+	    return new String(chars).substring(0, size);
 	}
 
 	public static void main(String[] args)
 	{
-		MyStringBuilder mysb = new MyStringBuilder();
-		System.out.println(mysb.length());
+		MyStringBuilder mysb = new MyStringBuilder("abc");
+		MyStringBuilder mysb1 = new MyStringBuilder("abc");
+		System.out.println(mysb.insert(1, mysb1));
 	}
 }
