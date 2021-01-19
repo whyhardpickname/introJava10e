011b813aa78651ca6cb2e5b2968112fca2989908
machern <13zxchen3@gmail.com>
2021-01-19 16:29:43 +0800

    通过dataIO类读写数据，利用bufferedIO类加速磁盘读写

--- ch17/exampleCode/TestDataStream.java
+++ ch17/exampleCode/TestDataStream.java
@@ -1,12 +1,8 @@
 package exampleCode;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
- * todo
  * 使用{@code DataInputStream}将学生的姓名和分数写入temp.dat,
  * 再用{@code DataOutputStream}读取出来。
  * @author MarkChern
@@ -19,11 +15,44 @@ public class TestDataStream
         String address = ".\\ch17\\exampleCode\\temp.dat";
 
         //写入数据
-        try (
+        try
+        (
             FileOutputStream fileOutputStream = new FileOutputStream(address);
-            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
-        ) {
+            //DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
+            //通过缓冲输出类加速文件输出
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+            DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
+        )
+        {
+            dataOutputStream.writeUTF("mark");
+            dataOutputStream.writeDouble(86.5);
 
+            dataOutputStream.writeUTF("alice");
+            dataOutputStream.writeDouble(96.5);
+        }
+
+        //读出数据并输出
+        try
+        (
+            FileInputStream fileInputStream = new FileInputStream(address);
+            //DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+            //通过缓冲输入类加速文件输入
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+        )
+        {
+            try
+            {
+                while (true)
+                {
+                    System.out.println(dataInputStream.readUTF());
+                    System.out.println(dataInputStream.readDouble());
+                }
+            }
+            catch (EOFException eofException)
+            {
+                System.out.println("All data were read.");
+            }
         }
     }
 }
