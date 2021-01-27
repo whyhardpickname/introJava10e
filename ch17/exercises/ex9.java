9b21c9af4803900f45825e9547bbc327070ea788
machern <13zxchen3@gmail.com>
2021-01-27 17:36:06 +0800

    编程地址簿

--- ch17/exercises/ex9.java
+++ ch17/exercises/ex9.java
@@ -1,8 +1,11 @@
 package exercises;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import com.sun.org.apache.regexp.internal.RE;
+
+import javax.xml.transform.Source;
+import java.io.*;
+import java.sql.ResultSet;
+import java.sql.SQLOutput;
 //TODO
 // - 测试
 /**
@@ -13,12 +16,17 @@ import java.io.RandomAccessFile;
  */
 public class ex9
 {
-    /**
-     * 地址簿
-     */
+    /** 指定字符串长度 */
+    final static int NAME_SIZE = 32;
+    final static int STREET_SIZE = 32;
+    final static int CITY_SIZE = 20;
+    final static int STATE_SIZE = 2;
+    final static int ZIP_SIZE = 5;
+    final static int RECORD_SIZE =
+            (NAME_SIZE + STREET_SIZE + CITY_SIZE + STATE_SIZE + ZIP_SIZE);
+
+    /** 地址簿文件 */
     RandomAccessFile addresses;
-    private long currentPos = 0;
-    private final int addressLength = 32 + 32 + 20 + 2 + 5;
 
     /**
      * {@code ex9}构造一个可读写的地址簿。
@@ -29,78 +37,183 @@ public class ex9
         addresses = new RandomAccessFile("ex17_09.txt", "rw");
     }
 
-    /**
-     * {@code add}方法添加一个新的地址。
-     * @param name
-     *      姓名
-     * @param street
-     *      街道
-     * @param city
-     *      城市
-     * @param state
-     *      州
-     * @param zip
-     *      邮编
-     */
-    public void add(String name, String street, String city, String state, String zip) throws IOException
+    /** 读取指定位置地址数据 */
+    public Address readAddress(long position) throws IOException
     {
-        String address = String.format("%.32s", name) +
-                String.format("%.32s", street) +
-                String.format("%.20s", city) +
-                String.format("%.2s", state) +
-                String.format("%.5s", zip);
-        addresses.writeUTF(address);
+        addresses.seek(position);
+        Address address = new Address(
+                FixedLengthStringIO.readFixedLengthString(NAME_SIZE, addresses),
+                FixedLengthStringIO.readFixedLengthString(STREET_SIZE, addresses),
+                FixedLengthStringIO.readFixedLengthString(CITY_SIZE, addresses),
+                FixedLengthStringIO.readFixedLengthString(STATE_SIZE, addresses),
+                FixedLengthStringIO.readFixedLengthString(ZIP_SIZE, addresses));
+        return address;
     }
 
-    /**
-     * {@code first}方法将当前页移动到首地址页，并打印首地址。
-     * @return
-     *      首地址
-     */
+    /** 写地址数据到指定位置 */
+    public void writeAddress(long position, Address address) throws IOException
+    {
+        addresses.seek(position);
+        FixedLengthStringIO.writeFixedLengthString(address.getName(), NAME_SIZE, addresses);
+        FixedLengthStringIO.writeFixedLengthString(address.getStreet(), STREET_SIZE, addresses);
+        FixedLengthStringIO.writeFixedLengthString(address.getCity(), CITY_SIZE, addresses);
+        FixedLengthStringIO.writeFixedLengthString(address.getState(), STATE_SIZE, addresses);
+        FixedLengthStringIO.writeFixedLengthString(address.getZip(), ZIP_SIZE, addresses);
+    }
+
+    /** 打印首地址 */
     public void first() throws IOException
     {
-        addresses.seek(0);
-        currentPos = 0;
-        System.out.println(addresses.readUTF());
+        if (addresses.length() > 0)
+        {
+            System.out.println(readAddress(0));
+        }
+        else
+        {
+            System.out.println("The address book is empty.");
+        }
     }
 
-    /**
-     * {@code next}方法将当前页移动到下一页，并打印下一页地址。
-     */
-    public String next() throws IOException
-    {
-        currentPos += addressLength;
-        System.out.println(addresses.readUTF());
-        return null;
-    }
-
-    /**
-     * {@code previous}方法将当前页移动到下一页，并打印上一页地址。
-     */
-    public String previous() throws IOException
-    {
-        currentPos -= (32 + 32 + 20 + 2 + 5);
-        addresses.seek(currentPos);
-        System.out.println(addresses.readUTF());
-        return null;
-    }
-
-    /**
-     * {@code last}方法将当前页移动到首地址页，并打印最后一个地址。
-     */
+    /** 打印末尾地址 */
     public void last() throws IOException
     {
-        currentPos = addresses.length() - addressLength;
-        System.out.println(addresses.readUTF());
+        if (addresses.length() - RECORD_SIZE >= 0)
+        {
+            System.out.println(readAddress(addresses.length() - 2 * RECORD_SIZE));
+        }
+        else
+        {
+            System.out.println("The address book is empty.");
+        }
     }
 
-    /**
-     * {@code update}方法更新当前页地址。
-     */
-    public void update(String name, String street, String city, String state, String zip) throws IOException
+    /** 下一个地址 */
+    public void next() throws IOException
     {
-        add(name, street, city, state, zip);
-        currentPos -= addressLength;
-        addresses.seek(currentPos);
+        if (addresses.getFilePointer() < addresses.length())
+        {
+            System.out.println(readAddress(addresses.getFilePointer()));
+        }
+        else
+        {
+            System.out.println("reach the end of the addresses.");
+        }
+    }
+
+    /** 上一个地址 */
+    public void previous() throws IOException
+    {
+        if (addresses.getFilePointer() - 2 * RECORD_SIZE >= 0)
+        {
+            System.out.println(readAddress(addresses.getFilePointer() - 2 * RECORD_SIZE));
+        }
+        else
+        {
+            System.out.println("reach the begin of the addresses.");
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        ex9 addresses = new ex9();
+        String s = "1";
+        addresses.writeAddress(0, new Address(s, s, s, s, s));
+        s = "2";
+        addresses.writeAddress(RECORD_SIZE, new Address(s, s, s, s, s));
+        s = "3";
+        addresses.writeAddress(2 * RECORD_SIZE, new Address(s, s, s, s, s));
+
+        addresses.first();
+        addresses.next();
+        addresses.next();
+        addresses.next();
+    }
+}
+
+/**
+ * {@code FixedLengthStringIO}提供读写固定长度的字符串的方法。
+ */
+class FixedLengthStringIO
+{
+    /** 从指定文件中读取固定长度的字符串 */
+    public static String readFixedLengthString(int size, DataInput in) throws IOException
+    {
+        char[] chars = new char[size];
+        for (int i = 0; i < size; i++)
+        {
+            chars[i] = in.readChar();
+        }
+        return new String(chars);
+    }
+
+    /** 写入固定长度的字符串到指定文件 */
+    public static void writeFixedLengthString(String s, int size, DataOutput out) throws IOException
+    {
+        char[] chars = new char[size];
+        //将字符串转换成数组
+        s.getChars(0, s.length(), chars, 0);
+        //如数组长度不足，使用空格填充
+        for (int i = Math.min(s.length(), size); i < size; i++)
+        {
+           chars[i] = ' ';
+        }
+        //写入字符串
+        out.writeChars(new String(chars));
+    }
+}
+
+/** 地址 */
+class Address
+{
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Address(String name, String street, String city, String state, String zip)
+    {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getStreet()
+    {
+        return street;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public String getZip()
+    {
+        return zip;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Address{" +
+                "name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
     }
 }
